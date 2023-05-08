6a666e1c074a102f72874c089b1d655ad034d39a
volodymyr.gryshko <volodymyr.gryshko@nosto.com>
2023-05-08 20:04:21 +0300

    Remove console debug

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,4 @@
 let currentUrl = window.location.href
 const param = "nostodebug=true"
 let newUrl = currentUrl.includes("?") ? currentUrl + "&" + param : currentUrl + "?" + param
-console.log("hello " + newUrl)
 window.location.href = newUrl
